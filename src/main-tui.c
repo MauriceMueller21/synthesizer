a354f3b2f27c26927253ffa682b644137026aaf3
Maurice Mueller <mathecrack52@gmail.com>
2022-04-10 22:37:11 +0200

    Fixed struct bug

--- src/main-tui.c
+++ src/main-tui.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
 	char* string2 = core.bool_to_string(false);
 	printf("%s\n", string1);
 	printf("%s\n", string2);
-	printf("%s\n", core.args.is_flag(argc, argv, 'a') ? "true" : "false");
+	printf("%s\n", tui.args.is_flag(argc, argv, 'a') ? "true" : "false");
 	printf("%s\n", core.bool_to_string(tui.args.is_flag(argc, argv, 'a')));
 	printf("Hello World! (%d parameters, %p is the pointer)\n", argc, argv);
 	return 0;
